259f83be32b440830105ab34f7729f5fd6ee62ee
Joshua Cold <joshua.cold@securitymetrics.com>
2025-08-30 17:11:58 -0600

    test(treesitter): add test for treesitter wrap cursor commands

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,28 +1,37 @@
 -- Add current directory to 'runtimepath' to be able to use 'lua' files
 vim.cmd([[let &rtp.=','.getcwd()]])
 
--- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
-if #vim.api.nvim_list_uis() == 0 then
-  -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
-  -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
-  --
-  local runtime_dependencies = {
-    "deps/mini.nvim",
-    "deps/nvim-treesitter",
-    "deps/neotest",
-    "deps/neotest-python",
-    "deps/nvim-dap",
-    "deps/nvim-dap-python",
-    "deps/nvim-lspconfig",
-    "deps/LuaSnip",
-  }
-  local runtime_path = vim.fn.join(runtime_dependencies, ",")
-  vim.cmd("set rtp+=" .. runtime_path)
+local runtime_dependencies = {
+  "deps/mini.nvim",
+  "deps/nvim-treesitter",
+  "deps/neotest",
+  "deps/neotest-python",
+  "deps/nvim-dap",
+  "deps/nvim-dap-python",
+  "deps/nvim-lspconfig",
+  "deps/LuaSnip",
+}
+local runtime_path = vim.fn.join(runtime_dependencies, ",")
+vim.cmd("set rtp+=" .. runtime_path)
 
-  -- Set up 'mini.test'
-  require("luasnip.extras.fmt")
-  require("luasnip.nodes.absolute_indexer")
-  require("nvim-treesitter.locals")
-  require("mini.test").setup()
-  require("mini.doc").setup()
-end
+-- Set up 'mini.test'
+require("luasnip.extras.fmt")
+require("luasnip.nodes.absolute_indexer")
+require("nvim-treesitter.locals")
+require("nvim-treesitter").setup()
+require("mini.test").setup()
+require("mini.doc").setup()
+require("nvim-treesitter.configs").setup({
+  modules = {
+    "highlight",
+  },
+  sync_install = false,
+  auto_install = true,
+  ignore_install = {},
+  ensure_installed = {
+    "python"
+  },
+  highlight = {
+    enable = true
+  }
+})
---
+++ tests/test_treesitter.lua
@@ -0,0 +1,59 @@
+-- Define helper aliases
+local new_set = MiniTest.new_set
+local expect, eq = MiniTest.expect, MiniTest.expect.equality
+
+-- Create (but not start) child Neovim object
+local child = MiniTest.new_child_neovim()
+
+-- Define main test set of this file
+local T = new_set({
+  -- Register hooks
+  hooks = {
+    -- This will be executed before every (even nested) case
+    pre_case = function()
+      -- Restart child process with custom 'init.lua' script
+      child.restart({ "-u", "scripts/minimal_init.lua" })
+      -- Load tested plugin
+      child.lua([[require('python').setup()]])
+    end,
+    -- This will be executed one after all tests from this set are finished
+    post_once = child.stop,
+  },
+})
+
+
+local get_lines = function() return child.api.nvim_buf_get_lines(0, 0, -1, true) end
+
+T['wrap_cursor'] = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      child.cmd("e _not_existing_new_buffer.py")
+      child.type_keys("cc", [["TEST"]], "<Esc>", "0")
+    end,
+  }
+})
+
+T['wrap_cursor']['normal'] = function()
+  child.cmd("Python treesitter wrap_cursor test(%s)")
+  eq(get_lines(), {[[test("TEST")]]})
+end
+
+T['wrap_cursor']['visual'] = function()
+  child.type_keys("l", "v", "$")
+  child.type_keys([[:Python treesitter wrap_cursor test(%s)<cr>]])
+  eq(get_lines(), {[["test(TEST")]]})
+end
+
+T['wrap_cursor']['visual_with_selection'] = function()
+  child.type_keys("l", "v", "$")
+  child.type_keys([[:Python treesitter wrap_cursor<cr>1<cr>]])
+  eq(get_lines(), {[["print(TEST")]]})
+end
+
+T['wrap_cursor']['with_selection'] = function()
+  child.type_keys([[:Python treesitter wrap_cursor<cr>1<cr>]])
+  eq(get_lines(), {[[print("TEST")]]})
+end
+
+-- Return test set which will be collected and execute inside `MiniTest.run()`
+return T
